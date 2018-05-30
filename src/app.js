7bf975fb17c2e33a9b0e3079305e9e94763eda6a
Jon Maldia <jon@maldia.net>
2018-05-29 22:56:31 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is Running');
+
+// JSX - JavaScript XML
+let template = <h1>Indecision App</h1>;
+
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
