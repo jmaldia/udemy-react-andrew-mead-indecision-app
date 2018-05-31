9cf48527a537bed2bc2fda6a1d263a15821a664e
Jon Maldia <jon@maldia.net>
2018-05-30 21:12:54 -0400

    Adds Exercises for learning more JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,79 @@
 
 console.log('App.js is Running');
 
+var app = {
+    title: 'The Awesome Indecision App',
+    subTitle: 'What a wonderful App'
+};
+
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subTitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Three'
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
 
+// Create Template JSX Expression
+// Render instead of Template
+var user = {
+    name: 'Jon',
+    age: 42,
+    location: 'NYC'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name.toUpperCase() + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
+var appRootTwo = document.getElementById('app-two');
+
 ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRootTwo);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 console.log('App.js is Running');
 
+let app = {
+    title: 'The Awesome Indecision App',
+    subTitle: 'What a wonderful App'
+};
+
 // JSX - JavaScript XML
-let template = <h1>Indecision App</h1>;
+let template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subTitle}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+            <li>Item Three</li>
+        </ol>
+    </div>
+);
 
 let appRoot = document.getElementById('app');
 
+// Create Template JSX Expression
+// Render instead of Template
+let user = {
+    name: 'Jon',
+    age: 42,
+    location: 'NYC'
+};
+
+let templateTwo = (
+    <div>
+        <h1>{user.name.toUpperCase() + '!'}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+let appRootTwo = document.getElementById('app-two');
+
 ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRootTwo);
