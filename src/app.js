1cce7951ebb38bd38ba351c73a077d0cb0d03bec
Jon Maldia <jon@maldia.net>
2018-07-05 22:36:08 -0400

    Adds import for npm modules

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@
 // console.log(add(4, 5));
 // console.log(subtract(4, 5));
 
+import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const template = <p>test</p>;
+const template = React.createElement('p', {}, 'testing 123');
+
+ReactDOM.render(template, document.getElementById('app'));
+
+console.log('email? ', validator.isEmail('jon.actester@gmail.com'));
+
 import isSenior, { isAdult, canDrink } from './person.js';
 console.log(isAdult(21));
 console.log(canDrink(21));
